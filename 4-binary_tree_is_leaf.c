fa6d47f20a797f020e5d717509e96b35aaef1139
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-27 22:34:18 +0300

    check if leaf2

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,8 +7,13 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node && (node->left || node->right))
-		return (0);
+	if (node)
+	{
+		if (node->left || node->right)
+			return (0);
+		else
+			return (1);
+	}
 	else
 		return (1);
 }
