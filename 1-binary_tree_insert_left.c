423076721fa1db179984a44ad1715f5ed2a7922c
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-27 17:04:07 +0300

    right - left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to previase node
+ * @value: intger value that will be in the node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+    
+	if (!new_node)
+		return (new_node);
+
+    if(!parent)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    new_node->left = parent->left;
+    parent->left = new_node;
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->right = NULL;
+    
+    
+	return (new_node);
+
+}
