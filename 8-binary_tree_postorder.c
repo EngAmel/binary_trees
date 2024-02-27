496386e79d049c7aefd25cd2a062ec9329af01eb
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-28 01:40:51 +0200

    task 8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -13,10 +13,10 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	if (tree->left)
-		binary_tree_inorder(tree->left, func);
+		binary_tree_postorder(tree->left, func);
 
 	if (tree->right)
-		binary_tree_inorder(tree->right, func);
+		binary_tree_postorder(tree->right, func);
 
 	if (func)
 		func(tree->n);
