7bab7c9288bd357fb4c10a22ade8d2277ec8bd99
EngAmel <amel.abdululazim.se@gmail.com>
2024-02-28 16:58:01 +0300

    perfict

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -28,9 +28,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 				return (1);
 			return (binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right));
-
-
 		}
+		else
+			return (0);
+
 	}
 	return (0);
 }
