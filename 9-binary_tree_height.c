de353931a7d30b7c57e276038d436e2eb9ab0da6
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-28 03:40:18 +0200

    task 11

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,9 +15,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (0);
-	left = binary_tree_height(tree->left) + 1;
-	right = binary_tree_height(tree->right) + 1;
-	return (max(left, right));
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (max(left, right) + 1);
 }
 /**
  * max - fiunction that return the maximum value between two numbers
