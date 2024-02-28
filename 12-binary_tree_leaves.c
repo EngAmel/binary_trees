29091a1ce421d47a0f2eb216689b03f9c38492c2
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-28 04:11:51 +0200

    task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -16,16 +16,18 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree->left && !tree->right)
 		return (1);
 
-	else if (tree->right && tree->left)
+	if (tree->right && tree->left)
 	{
 		left = binary_tree_leaves(tree->left);
 		right = binary_tree_leaves(tree->right);
 		return (left + right);
 	}
 
-	else if (tree->right && !tree->left)
+	if (tree->right && !tree->left)
 		return (binary_tree_leaves(tree->right));
 
-	else if (tree->left && !tree->right)
+	if (tree->left && !tree->right)
 		return (binary_tree_leaves(tree->left));
+
+	return (0);
 }
