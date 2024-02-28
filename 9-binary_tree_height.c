3e8292ba9ce42db5bb69ad1a03fde55d1aa4562e
mahmoud-5555 <mahmoudadam5555@gmail.com>
2024-02-28 02:12:38 +0200

    task 8

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_height - Write a function that measures
+ * the height of a binary tree
+ * @tree: the root of the tree
+ * Return: the hight of the tree
+*/
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (max(left, right) + 1);
+}
+/**
+ * max - fiunction that return the maximum value between two numbers
+ * @first_number: first number
+ * @secand_number: secand number
+ * Return: the greatast value
+*/
+size_t max(size_t first_number, size_t secand_number)
+{
+	if (first_number >= secand_number)
+		return (first_number);
+	else
+		return (secand_number);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -27,7 +27,7 @@ typedef struct binary_tree_s heap_t;
 
 
 /*requaired function*/
-
+size_t max(size_t first_number, size_t secand_number);
 /*tasks function*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -38,6 +38,6 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
-
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif
